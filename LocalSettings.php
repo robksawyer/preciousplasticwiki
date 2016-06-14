3a0d0274eb752fef021ec951f8d5bd6d8264e8d6
Rob Sawyer <robksawyer@gmail.com>
2016-06-13 19:20:37 -0700

    Added a PDF embed extension

--- LocalSettings.php
+++ LocalSettings.php
@@ -299,6 +299,14 @@ require_once("$IP/extensions/ContributionTracking/ContributionTracking.php");
 
 require_once "$IP/extensions/Memcached/Memcached.php";
 
+require_once "$IP/extensions/PDFEmbed/PDFEmbed.php";
+
+//Default width for the PDF object container.
+$pdfEmbed['width'] = 800;
+
+//Default height for the PDF object container.
+$pdfEmbed['height'] = 1090;
+
 ##
 # @url https://www.mediawiki.org/wiki/Extension:DonationInterface
 ##
