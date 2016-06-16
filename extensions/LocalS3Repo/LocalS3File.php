ed5a0f7aca1f20df1666bf5ef512f48354718de0
Rob Sawyer <robksawyer@gmail.com>
2016-06-15 22:29:06 -0700

    Fixed a bug in LocalS3File

--- extensions/LocalS3Repo/LocalS3File.php
+++ extensions/LocalS3Repo/LocalS3File.php
@@ -238,7 +238,7 @@ class LocalS3File extends File {
 	 * Load metadata from the file itself
 	 */
 	function loadFromFile() {
-		$this->setProps( File::getPropsFromPath( $this->getPath() ) );
+		$this->setProps( $this->getPropsFromPath( $this->getPath() ) );
 	}
 
 	function getCacheFields( $prefix = 'img_' ) {
