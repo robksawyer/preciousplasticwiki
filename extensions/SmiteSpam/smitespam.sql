19b2beb182ad76f96b884c2c54279879e98ffd44
Rob Sawyer <robksawyer@gmail.com>
2016-08-04 09:46:42 -0700

    Converted script to pgsql

--- extensions/SmiteSpam/smitespam.sql
+++ extensions/SmiteSpam/smitespam.sql
@@ -3,13 +3,13 @@
 --
 CREATE TABLE /*_*/smitespam_trusted_user (
 	-- User ids of trusted users
-	trusted_user_id int unsigned NOT NULL,
+	trusted_user_id int check (trusted_user_id > 0) NOT NULL,
 
 	-- Timestamp of when a user was marked as trusted
-	trusted_user_timestamp binary(14) NOT NULL default '',
+	trusted_user_timestamp bytea NOT NULL default '',
 
 	-- User ID of admin who marked a user as trusted
-	trusted_user_admin_id int unsigned NOT NULL
+	trusted_user_admin_id int check (trusted_user_admin_id > 0) NOT NULL
 ) /*$wgDBTableOptions*/;
 
 CREATE UNIQUE INDEX /*i*/trusted_user_id ON /*_*/smitespam_trusted_user (trusted_user_id);
