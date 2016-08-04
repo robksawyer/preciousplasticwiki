834e3136f47f6e3dd7c647e2947a6a7d496c7f7a
Rob Sawyer <robksawyer@gmail.com>
2016-08-04 11:25:01 -0700

    Updated the contribution patch

--- extensions/ContributionTracking/patches/patch-owa.sql
+++ extensions/ContributionTracking/patches/patch-owa.sql
@@ -1,13 +1,14 @@
 --
 -- create contribution_tracking.owa_session and owa_ref
 --
+-- BEGIN
+-- 	ALTER TABLE /*_*/contribution_tracking ADD owa_session VARCHAR(255) default NULL;
+-- EXCEPTION
+-- 	WHEN owa_session THEN RAISE NOTICE 'column <owa_session> already exists in contribution_tracking.';
+-- END;
+-- BEGIN
+-- 	ALTER TABLE /*_*/contribution_tracking ADD owa_ref INTEGER default NULL;
+-- EXCEPTION
+-- 	WHEN owa_ref THEN RAISE NOTICE 'column <owa_session> already exists in contribution_tracking.';
 BEGIN
-	ALTER TABLE /*_*/contribution_tracking ADD owa_session VARCHAR(255) default NULL;
-EXCEPTION
-	WHEN owa_session THEN RAISE NOTICE 'column <owa_session> already exists in contribution_tracking.';
-END;
-BEGIN
-	ALTER TABLE /*_*/contribution_tracking ADD owa_ref INTEGER default NULL;
-EXCEPTION
-	WHEN owa_ref THEN RAISE NOTICE 'column <owa_session> already exists in contribution_tracking.';
 END;
