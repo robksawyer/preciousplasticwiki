a8df18cd65538915114f0ac8faad2e6bddcf8b01
Rob Sawyer <robksawyer@gmail.com>
2016-08-04 10:04:49 -0700

    More postgres updates for contribution tracking

--- extensions/ContributionTracking/patches/patch-owa.sql
+++ extensions/ContributionTracking/patches/patch-owa.sql
@@ -1,5 +1,5 @@
 --
 -- create contribution_tracking.owa_session and owa_ref
 --
-ALTER TABLE /*_*/contribution_tracking ADD owa_session varchar(255) default NULL;
-ALTER TABLE /*_*/contribution_tracking ADD owa_ref integer(11) default NULL;
+ALTER TABLE /*_*/contribution_tracking ADD owa_session VARCHAR(255) default NULL;
+ALTER TABLE /*_*/contribution_tracking ADD owa_ref INTEGER default NULL;
