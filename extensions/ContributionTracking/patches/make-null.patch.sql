40ffe381e39e3d7fd1faba6b14a6f6a29293f1bc
Rob Sawyer <robksawyer@gmail.com>
2016-08-04 11:36:55 -0700

    Updated the contribution patch

--- extensions/ContributionTracking/patches/make-null.patch.sql
+++ extensions/ContributionTracking/patches/make-null.patch.sql
@@ -1,4 +1,4 @@
 --
 -- Make two things default to NULL
 --
-alter table /*_*/contribution_tracking modify anonymous tinyint(1) unsigned default null, modify optout tinyint(1) unsigned default null;
+-- alter table /*_*/contribution_tracking modify anonymous tinyint(1) unsigned default null, modify optout tinyint(1) unsigned default null;
